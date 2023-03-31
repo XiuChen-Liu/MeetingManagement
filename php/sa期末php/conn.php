a98a8816e40635862407b67a112e1939789dee99
XiuChen <george46174603@gmail.com>
2023-03-31 23:35:33 +0800

    update

--- php/sa期末php/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_name = "dbsa";
-$username = "root";
-$password = "1234";
-$servename = "localhost";
-$conn = mysqli_connect($servename ,$username ,$password ,$db_name );
-mysqli_set_charset($conn ,"utf-8");
-
-?>
